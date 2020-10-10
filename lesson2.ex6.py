e22b90c79bac31c419f671cc76c230f908249291
sixsymbols <48753422+sixsymbols@users.noreply.github.com>
2020-10-10 17:04:44 +0700

    Add files via upload

---
+++ lesson2.ex6.py
@@ -0,0 +1,15 @@
+goods = int(input("кол-во товара "))
+n = 1
+my_dict = []
+my_list = []
+my_analys = {}
+while n <= goods:
+    my_dict = dict({'название': input("введите название "), 'цена': input("цена"),
+                    'количество': input('кол-во'), 'eд': input("ед.измерения ")})
+    my_list.append((n, my_dict))
+    n += 1
+    my_analys = dict(
+        {'название': my_dict.get('название'), 'цена': my_dict.get('цена'), 'количество': my_dict.get('количество'),
+         'ед': my_dict.get('ед')})
+print(my_list)
+print(my_analys)
